9afb3000706bd40661b71598eff42fb1fe075ab4
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2020-01-13 20:14:11 +0000

    New tests for default ROI implementation

---
+++ tests/testthat/test-separator_ROI.R
@@ -0,0 +1,23 @@
+context("ROI implementation")
+
+
+if (requireNamespace("AER", quietly = TRUE)) {
+    data("MurderRates", package = "AER")
+    murder_formula <- I(executions > 0) ~ time + income + noncauc + lfp + southern
+    murder_sep_lpsolve <- glm(murder_formula, data = MurderRates,
+                              family = binomial(),
+                              method = "detect_separation",
+                              implementation = "ROI",
+                              solver = "lpsolve")
+
+    murder_sep_glpk <- update(murder_sep_lpsolve,
+                              implementation = "ROI",
+                              solver = "glpk")
+
+    
+    test_that("ROI implementation returns the same result with solver lpsolve and solve glpk", {
+        expect_equal(coef(murder_sep_konis), coef(murder_sep_glpk))    
+    })    
+
+}
+
