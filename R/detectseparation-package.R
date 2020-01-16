5f2c09c6876276e1bdcd4ea60fe0e0ed6faba5c3
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2020-01-16 20:44:35 +0000

    Added link to check_infinite_estimates.glm

--- R/detectseparation-package.R
+++ R/detectseparation-package.R
@@ -12,6 +12,8 @@ NULL
 #' @param object a fitted model object (e.g. the result of a
 #'     \code{\link{glm}} call).
 #' @param ... other options to be passed to the method.
+#'
+#' @seealso check_infinite_estimates.glm
 #' @export
 check_infinite_estimates <- function(object, ...) {
     UseMethod("check_infinite_estimates")
