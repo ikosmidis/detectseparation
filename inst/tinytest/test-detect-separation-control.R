33aedb6492e104d978b7fe2f5e46cadee3a85ec3
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2022-03-26 09:52:32 +0000

    extra tests for detect_separation_control

--- inst/tinytest/test-detect-separation-control.R
+++ inst/tinytest/test-detect-separation-control.R
@@ -7,3 +7,14 @@ expect_equal(out0$purpose, "find")
 expect_equal(out0$tolerance, 1e-04)
 expect_equal(out0$separator, detectseparation::separator_ROI)
 expect_equal(out0$implementation, "ROI")
+
+## test arguments
+out1 <- detect_separation_control(implementation = "lpSolveAPI", solver = "alabama", linear_program = "dual", solver_control = list(abs = 2), tolerance = 1e-07)
+
+expect_equal(out1$linear_program, "dual")
+expect_equal(out1$solver, "alabama")
+expect_equal(out1$purpose, "find")
+expect_equal(out1$tolerance, 1e-07)
+expect_equal(out1$separator, detectseparation::separator_lpSolveAPI)
+expect_equal(out1$implementation, "lpSolveAPI")
+
